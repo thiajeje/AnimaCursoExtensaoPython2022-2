493590c238178a7139f98617e5c878f6899b9dc3
Thiago Ajeje <63625421+thiajeje@users.noreply.github.com>
2022-11-02 02:17:01 +0000

    Aprendi a criar variáveis e exibi-las na tela!

--- main.py
+++ main.py
@@ -4,6 +4,14 @@
 
 print("Alo mundo!")
 
+# Quando quiser guardar uma String!
+nome = "Thiago Ajeje"
+
+# Quando quiser guardar um número inteiro
+idade = 29
+
+# Exibir o meu nome (que está dentro da variável nome)
+print(nome)
 
 '''
 Comentario em bloco
